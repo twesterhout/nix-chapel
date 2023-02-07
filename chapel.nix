17b42d2fe7eacc13bca6fc84b15e4a660e936e6f
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2023-02-07 19:45:53 +0100

    Seems to build

--- chapel.nix
+++ chapel.nix
@@ -1,8 +1,4 @@
-{ stdenv
-, clangStdenv
-, clang
-, llvmPackages
-, lib
+{ llvmPackages
 , fetchFromGitHub
 , python39
 , bash
@@ -14,32 +10,34 @@ llvmPackages.stdenv.mkDerivation rec {
   pname = "chapel";
   version = "1.29.0";
 
-  src = ./chapel;
-  # fetchFromGitHub {
-  #   owner = "chapel-lang";
-  #   repo = "chapel";
-  #   rev = "1.29.0";
-  #   sha256 = "sha256-46JMbZUDjVVEdxL5uHMG3XIoUzpZMzjTX4KEKEbBsm0=";
-  # };
+  # src = ./chapel;
+  src = fetchFromGitHub {
+    owner = "chapel-lang";
+    repo = "chapel";
+    rev = "1.29.0";
+    sha256 = "sha256-46JMbZUDjVVEdxL5uHMG3XIoUzpZMzjTX4KEKEbBsm0=";
+  };
+
+  patches = [ ./llvm_config.patch ];
 
   configurePhase = ''
-    export CHPL_LLVM=system
-    export CHPL_HOST_COMPILER=llvm
-    export CHPL_HOST_CC=$(which clang)
-    export CHPL_HOST_CXX=$(which clang++)
-
-    export CHPL_TARGET_CPU=
-    export PREFIX=$out
-
     patchShebangs --build configure
     patchShebangs --build util/printchplenv
     patchShebangs --build util/config/compileline
+
+    export CHPL_LLVM=system
+    export CHPL_HOST_COMPILER=llvm
+    export CHPL_HOST_CC=${llvmPackages.clang}/bin/clang
+    export CHPL_HOST_CXX=${llvmPackages.clang}/bin/clang++
+    export CHPL_TARGET_CPU=
     ./configure --prefix=$out
   '';
 
   buildPhase = ''
-    make -j
+    make -j4
   '';
+
+  doCheck = true;
 
   buildInputs = [
     llvmPackages.clang
@@ -49,13 +47,12 @@ llvmPackages.stdenv.mkDerivation rec {
 
   nativeBuildInputs = [
     bash
-    which
     python39
     gnustep.make
     llvmPackages.clang
   ];
 
-  meta = with lib; {
+  meta = {
     description = "a Productive Parallel Programming Language";
     homepage = "https://chapel-lang.org/";
   };
