053c94c205eda3670a4df94704cee13c873c2943
twesterhout <14264576+twesterhout@users.noreply.github.com>
2023-05-31 11:14:55 +0200

    Split chapelFixupBinary into a separate file/derivation

--- chapel.nix
+++ chapel.nix
@@ -1,33 +1,29 @@
-{ llvmPackages_14
-, gmp
-, gcc-unwrapped
+{ bash
+, cmake
 , coreutils
-, xz
-, libunwind
 , fetchFromGitHub
-, python39
-, bash
+, file
+, gcc-unwrapped
+, gmp
 , gnumake
 , gnum4
-, which
-, cmake
-, mpi
-, rdma-core
-, pkg-config
+, libunwind
+, llvmPackages_14
 , makeWrapper
+, mpi
 , perl
+, pmix
+, python39
+, pkg-config
+, rdma-core
+, which
+, xz
 }:
 
-llvmPackages_14.stdenv.mkDerivation {
+llvmPackages_14.stdenv.mkDerivation rec {
   pname = "chapel";
-  version = "1.30.0";
+  version = "1.31.0";
 
-  # src = fetchFromGitHub {
-  #   owner = "bradcray";
-  #   repo = "chapel";
-  #   rev = "build-amudprun-host-with-host-cc";
-  #   sha256 = "sha256-6Ijg8vfeozxCrEp9ZyWg9lf5bdB8+DDNBQTQU126VJ8=";
-  # };
   src = fetchFromGitHub {
     owner = "chapel-lang";
     repo = "chapel";
@@ -35,19 +31,17 @@ llvmPackages_14.stdenv.mkDerivation {
     sha256 = "sha256-fPYEdLaI34L95vxPo321NLpItso1UaHYUyJvQyx+IBw=";
   };
 
+  outputs = [ "out" "third_party" ];
+
   patches = [ ./llvm-and-clang-paths.patch ];
   postPatch = ''
-    # substituteInPlace third-party/gasnet/gasnet-src/other/amudp/Makefile.common \
-    #   --replace 'CC = gcc' 'CC = cc' \
-    #   --replace 'CXX = g++' 'CXX = c++'
-  '';
-
-  configurePhase = ''
     patchShebangs --build configure
     patchShebangs --build util/printchplenv
     patchShebangs --build util/config/compileline
     patchShebangs --build util/test/checkChplInstall
+  '';
 
+  configurePhase = ''
     export CC=${llvmPackages_14.clang}/bin/cc
     export CXX=${llvmPackages_14.clang}/bin/c++
     export CHPL_LLVM=system
@@ -61,45 +55,40 @@ llvmPackages_14.stdenv.mkDerivation {
     export CHPL_GMP=system
     export CHPL_RE2=bundled
     export CHPL_UNWIND=system
+    export PMI_HOME=${pmix}
 
     ./configure --chpl-home=$out
   '';
 
   buildPhase = ''
-    make -j
+    # make -j
+    for CHPL_LIB_PIC in none pic; do
+      make CHPL_LIB_PIC=$CHPL_LIB_PIC -j
+    done
     make CHPL_COMM=gasnet CHPL_COMM_SUBSTRATE=smp -j
     make CHPL_COMM=gasnet CHPL_COMM_SUBSTRATE=udp -j
-    # make CHPL_COMM=gasnet CHPL_LAUNCHER=gasnetrun_mpi CHPL_COMM_SUBSTRATE=mpi -j
-    # make CHPL_COMM=gasnet CHPL_LAUNCHER=slurm-gasnetrun_mpi CHPL_COMM_SUBSTRATE=mpi -j
-    # make CHPL_COMM=gasnet CHPL_LAUNCHER=slurm-srun CHPL_COMM_SUBSTRATE=mpi -j
-    # make CHPL_COMM=gasnet CHPL_LAUNCHER=none CHPL_COMM_SUBSTRATE=mpi -j
-    make CHPL_COMM=gasnet CHPL_LAUNCHER=gasnetrun_ibv CHPL_COMM_SUBSTRATE=ibv -j
-    make CHPL_COMM=gasnet CHPL_LAUNCHER=slurm-gasnetrun_ibv CHPL_COMM_SUBSTRATE=ibv -j
-    make CHPL_COMM=gasnet CHPL_LAUNCHER=none CHPL_COMM_SUBSTRATE=ibv -j
-    # for CHPL_LAUNCHER in gasnetrun_ibv gasnetrun_mpi slurm-gasnetrun_ibv slurm-gasnetrun_mpi slurm-srun none; do
-    #   for CHPL_COMM_SUBSTRATE in smp mpi udp ibv; do
-    #     make CHPL_COMM=gasnet CHPL_LAUNCHER=$CHPL_LAUNCHER CHPL_COMM_SUBSTRATE=$CHPL_COMM_SUBSTRATE -j
-    #   done
-    # done
-    # for CHPL_COMM_SUBSTRATE in smp mpi udp ibv; do
-    #   make CHPL_COMM=gasnet CHPL_COMM_SUBSTRATE=$CHPL_COMM_SUBSTRATE -j
-    # done
-    # make CHPL_COMM=gasnet CHPL_COMM_SUBSTRATE=udp -j
-    # make CHPL_COMM=gasnet CHPL_COMM_SUBSTRATE=mpi -j
-    # make CHPL_COMM=gasnet CHPL_COMM_SUBSTRATE=smp -j
-    # for CHPL_LIB_PIC in none pic; do
-    #   for CHPL_UNWIND in none system; do
-    #     make  CHPL_UNWIND=$CHPL_UNWIND CHPL_LIB_PIC=$CHPL_LIB_PIC -j
-    #   done
-    # done
+    make CHPL_COMM=gasnet CHPL_COMM_SUBSTRATE=udp CHPL_LAUNCHER=none -j
+    for CHPL_LAUNCHER in none gasnetrun_mpi slurm-gasnetrun_mpi slurm-srun; do
+      make CHPL_COMM=gasnet CHPL_COMM_SUBSTRATE=mpi CHPL_LAUNCHER=$CHPL_LAUNCHER -j
+    done
+    for CHPL_LAUNCHER in none gasnetrun_ibv slurm-gasnetrun_ibv; do
+      make CHPL_COMM=gasnet CHPL_COMM_SUBSTRATE=ibv CHPL_LAUNCHER=$CHPL_LAUNCHER -j
+    done
   '';
 
   postInstall = ''
-    # --prefix PATH : "${mpi}/bin"
+    mkdir -p $third_party
+    cp -v -r $out/third-party/gasnet $third_party
+    find $third_party -type d -name "include" -exec rm -r {} +
+    find $third_party -type d -name "lib" -exec rm -r {} +
+    find $third_party -type d -name "share" -exec rm -r {} +
+    find $third_party -type f -name "Makefile*" -exec rm {} +
+
     makeWrapper $out/bin/linux64-x86_64/chpl $out/bin/chpl \
       --prefix PATH : "${pkg-config}/bin" \
       --prefix PATH : "${coreutils}/bin" \
       --prefix PATH : "${gnumake}/bin" \
+      --prefix PATH : "${mpi}/bin" \
       --prefix PATH : "${python39}/bin" \
       --prefix PKG_CONFIG_PATH : "${libunwind.dev}/lib/pkgconfig" \
       --set-default CHPL_HOME $out \
@@ -135,19 +124,9 @@ llvmPackages_14.stdenv.mkDerivation {
       --set-default CHPL_GMP system \
       --set-default CHPL_RE2 bundled \
       --set-default CHPL_UNWIND system
-  '';
 
-  checkPhase = ''
-    export PATH=$PWD/bin/linux64-x86_64:$PATH
-    wrapProgram bin/linux64-x86_64/chpl \
-      --add-flags "-I ${llvmPackages_14.bintools.libc.dev}/include" \
-      --add-flags "-I ${llvmPackages_14.clang-unwrapped.lib}/lib/clang/14.0.6/include" \
-      --add-flags "-L ${gmp}/lib" \
-      --add-flags "-L ${xz.out}/lib"
-    mv bin/linux64-x86_64/.chpl-wrapped bin/linux64-x86_64/chpl
+    ln -s $out/util/printchplenv $out/bin/
   '';
-
-  doCheck = false;
 
   buildInputs = [
     llvmPackages_14.clang
@@ -155,21 +134,23 @@ llvmPackages_14.stdenv.mkDerivation {
     llvmPackages_14.libclang.dev
     libunwind
     gmp
-    # mpi
+    mpi
+    pmix
     rdma-core
   ];
 
   nativeBuildInputs = [
     bash
-    python39
+    cmake
     gnumake
     gnum4
-    which
-    cmake
-    perl
-    pkg-config
+    file
     llvmPackages_14.clang
     makeWrapper
+    perl
+    pkg-config
+    python39
+    which
   ];
 
   meta = {
--- flake.nix
+++ flake.nix
@@ -17,36 +17,55 @@
   };
 
   outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
+    with builtins;
     let
       pkgs = import inputs.nixpkgs { inherit system; };
       chapel = pkgs.callPackage ./chapel.nix { };
+      chapelFixupBinary = with pkgs; writeShellScriptBin "chapelFixupBinary" ''
+        set -e
+
+        replaceReferencesWith() {
+          sed -i "s:$2:$3:g" "$1"
+        }
+
+        hideReferencesTo() {
+          declare -r filepath=$1
+          declare -r original=$2
+          declare -r new=$(echo "$original" | sed -E 's:/nix/store/[a-z0-9]{32}-:/nix/store/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX-:')
+          replaceReferencesWith "$filepath" "$original" "$new"
+        }
+
+        if [ $# -ne 1 ]; then
+          echo "Expected a single argument -- binary to fixup"
+          exit 1
+        fi
+
+        binaryFile=$1
+        hideReferencesTo "$binaryFile" ${llvmPackages_14.clang}
+        hideReferencesTo "$binaryFile" ${llvmPackages_14.clang-unwrapped.lib}
+        hideReferencesTo "$binaryFile" ${llvmPackages_14.llvm.dev}
+        hideReferencesTo "$binaryFile" ${llvmPackages_14.bintools.libc.dev}
+        replaceReferencesWith "$binaryFile" ${chapel}/third-party/ ${chapel.third_party}/
+        hideReferencesTo "$binaryFile" ${chapel}
+      '';
+
       hello-chapel = pkgs.stdenv.mkDerivation {
         name = "hello-chapel";
         version = "1.0.0";
         src = ./.;
         dontConfigure = true;
-        nativeBuildInputs = [ chapel ];
+        nativeBuildInputs = [ chapel chapelFixupBinary pkgs.coreutils ];
+        # disallowedReferences = [ pkgs.llvmPackages_14.clang ];
         buildPhase = ''
-          CHPL_COMM=gasnet CHPL_COMM_SUBSTRATE=ibv CHPL_LAUNCHER=none chpl hello6-taskpar-dist.chpl
+          chpl --print-commands --devel hello6-taskpar-dist.chpl
+          chapelFixupBinary hello6-taskpar-dist
         '';
         installPhase = ''
           mkdir -p $out/bin
           cp -v hello6-taskpar-dist $out/bin/
-          if [ -f hello6-taskpar-dist_real ]; then
-            cp -v hello6-taskpar-dist_real $out/bin/
-          fi
         '';
       };
-      hello-shell = pkgs.mkShell {
-        buildInputs = [ ];
-        nativeBuildInputs = with pkgs; [
-          hello
-          hello-chapel
-          bash
-          coreutils
-          binutils
-        ];
-      };
+
       hello-docker = pkgs.dockerTools.buildImage {
         name = "pre-sif-container";
         tag = "latest";
@@ -54,75 +73,27 @@
           Cmd = [ "${hello-chapel}/bin/hello6-taskpar-dist" "-v" "--numLocales=2" ];
         };
       };
-      # hello-docker = pkgs.dockerTools.buildNixShellImage {
-      #   name = "pre-sif-container";
-      #   tag = "latest";
-      #   drv = hello-shell;
-      # };
       hello-singularity = pkgs.singularity-tools.buildImage {
         name = "hello-singularity";
-        contents = with pkgs; [ coreutils binutils hello-chapel ];
+        contents = [ hello-chapel ];
         runScript = ''
           #!${pkgs.stdenv.shell}
 
-          export PATH=${hello-chapel}/bin:${pkgs.binutils}/bin:${pkgs.coreutils}/bin:$PATH
+          export PATH=${hello-chapel}/bin:$PATH
           exec /bin/sh
         '';
         diskSize = 10240;
         memSize = 5120;
       };
-      # hello-singularity = pkgs.stdenv.mkDerivation {
-      #   name = "container.sif";
-      #   src = ./.;
-      #   installPhase = '' 
-      #     mkdir unpack
-      #     echo "${hello-docker}"
-      #     tar xzvf ${hello-docker} -C unpack
-      #     # Singularity can't handle .gz
-      #     tar -C unpack/ -cvf layer.tar .
-      #     # TODO: Allow for module of user defined nightly, opposed to using src
-      #     singularity build --fakeroot $out Singularity.nightly
-      #   '';
-      #   nativeBuildInputs = [ pkgs.singularity ];
-      # };
     in
     {
       packages.default = chapel;
       packages.hello-chapel = hello-chapel;
-      packages.docker = hello-docker;
+      packages.hello-docker = hello-docker;
       packages.hello-singularity = hello-singularity;
       apps.default = {
         type = "app";
         program = "${chapel}/bin/chpl";
-      };
-      devShells.chapel = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          llvmPackages_14.clang
-          llvmPackages_14.llvm
-          llvmPackages_14.libclang.dev
-        ];
-        nativeBuildInputs = with pkgs; [
-          chapel
-          bash
-          python39
-          gnumake
-          gnum4
-          which
-          pkg-config
-          llvmPackages_14.clang-unwrapped
-          llvmPackages_14.clang-unwrapped.dev
-          llvmPackages_14.clang-unwrapped.lib.lib
-        ];
-        shellHook = ''
-          export CLANG=${pkgs.llvmPackages_14.clang}
-          export CLANG_UNWRAPPED=${pkgs.llvmPackages_14.clang-unwrapped}
-          export CLANG_UNWRAPPED_DEV=${pkgs.llvmPackages_14.clang-unwrapped.dev}
-          export STDENV_PATH=${pkgs.llvmPackages_14.stdenv.cc}
-          export LIBC_PATH=${pkgs.llvmPackages_14.bintools.libc}
-          export LIBC_DEV_PATH=${pkgs.llvmPackages_14.bintools.libc.dev}
-
-          export EXTRA_FLAGS='-I ${pkgs.llvmPackages_14.bintools.libc.dev}/include -I ${pkgs.llvmPackages_14.clang-unwrapped.lib}/lib/clang/14.0.6/include'
-        '';
       };
       devShells.default = (pkgs.mkShell.override { stdenv = pkgs.llvmPackages_14.stdenv; }) {
         packages = [ chapel ];
@@ -131,43 +102,38 @@
           llvmPackages_14.llvm
           llvmPackages_14.libclang.dev
           libunwind
+          gmp
+          mpi
+          pmix
+          rdma-core
         ];
         nativeBuildInputs = with pkgs; [
           bash
-          python39
+          cmake
           gnumake
           gnum4
-          which
-          cmake
+          file
+          llvmPackages_14.clang
+          makeWrapper
+          perl
           pkg-config
-          clang_14
-          llvmPackages_14.clang-unwrapped
-          llvmPackages_14.clang-unwrapped.dev
-          llvmPackages_14.clang-unwrapped.lib.lib
-          nil
-          nixpkgs-fmt
+          python39
+          which
         ];
-        shellHook = ''
-          patchShebangs --build chapel/configure
-          patchShebangs --build chapel/util/printchplenv
-          patchShebangs --build chapel/util/config/compileline
-          patchShebangs --build chapel/util/test/checkChplInstall
-
+        shellHook = with pkgs; ''
+          export CC=${llvmPackages_14.clang}/bin/cc
+          export CXX=${llvmPackages_14.clang}/bin/c++
           export CHPL_LLVM=system
-          export CHPL_LLVM_CONFIG=${pkgs.llvmPackages_14.llvm.dev}/bin/llvm-config
+          export CHPL_LLVM_CONFIG=${llvmPackages_14.llvm.dev}/bin/llvm-config
           export CHPL_HOST_COMPILER=llvm
-          export CHPL_HOST_CC=${pkgs.llvmPackages_14.clang}/bin/clang
-          export CHPL_HOST_CXX=${pkgs.llvmPackages_14.clang}/bin/clang++
+          export CHPL_HOST_CC=${llvmPackages_14.clang}/bin/clang
+          export CHPL_HOST_CXX=${llvmPackages_14.clang}/bin/clang++
           export CHPL_TARGET_CPU=none
-          export CHPL_TARGET_CC=${pkgs.llvmPackages_14.clang}/bin/clang
-          export CHPL_TARGET_CXX=${pkgs.llvmPackages_14.clang}/bin/clang++
+          export CHPL_TARGET_CC=${llvmPackages_14.clang}/bin/clang
+          export CHPL_TARGET_CXX=${llvmPackages_14.clang}/bin/clang++
           export CHPL_GMP=system
-          export CHPL_RE2=none
-
-          export UNWIND_PATH=${pkgs.llvmPackages_14.libunwind}
-          export XZ_PATH=${pkgs.xz}
-          export XZ_PATH_DEV=${pkgs.xz.dev}
-          export EXTRA_FLAGS='-I ${pkgs.llvmPackages_14.bintools.libc.dev}/include -I ${pkgs.llvmPackages_14.clang-unwrapped.lib}/lib/clang/14.0.6/include'
+          export CHPL_RE2=bundled
+          export CHPL_UNWIND=system
         '';
       };
       formatter = pkgs.nixpkgs-fmt;
